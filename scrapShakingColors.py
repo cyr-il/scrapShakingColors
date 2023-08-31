d5a4b12229af6201e0e121e9816f99b4e05122ac
KAYCORP\crufin <crufin@kayentis.com>
2023-08-31 19:09:27 +0200

    correction of bug for the last article available

--- scrapShakingColors.py
+++ scrapShakingColors.py
@@ -1,52 +1,69 @@
 import requests
 from bs4 import BeautifulSoup
-from translate import Translator
+from googletrans import Translator
 from datetime import datetime
 import dateparser
+import time
+import discord
 
 # Make a GET request to the webpage
 url = "https://www.shakingcolors.com/"
-response = requests.get(url)
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+most_recent_date = datetime.min
 
-# Find all the article elements on the page
-article_elements = soup.find_all(class_="card")
+while True :
+    # Initialize variables to store the most recent article and its publication date
+    response = requests.get(url)
 
-# Initialize variables to store the most recent article and its publication date
-most_recent_article = None
-most_recent_date = None
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
 
-# Iterate over the article elements and find the most recent article
-for article_element in article_elements:
-    # Extract the publication date of the article (replace with your own logic)
-    publication_date_element = article_element.find("div", class_="card__data")
-    publication_date = dateparser.parse(publication_date_element.text.strip())
+    # Find all the article elements on the page
+    article_elements = soup.find_all(class_="card")
 
-    # Compare the publication date with the current most recent date
-    if most_recent_date is None or publication_date > most_recent_date:
-        most_recent_article = article_element
-        most_recent_date = publication_date
+    most_recent_article = None
+    new_article_found = False
+        
+    # Iterate over the article elements and find the most recent article
+    for article_element in article_elements:
+        # Extract the publication date of the article (replace with your own logic)
+        publication_date_element = article_element.find("div", class_="card__data")
+        publication_date = dateparser.parse(publication_date_element.text.strip())
+        
+        
+        # Compare the publication date with the current most recent date
+        if  publication_date > most_recent_date:
+            most_recent_article = article_element
+            most_recent_date = publication_date
+            new_article_found = True
 
-# Extract the link to the full article
-article_link = most_recent_article.find("a")["href"]
+    if new_article_found:
+        print("New article found published on ", most_recent_date)
+        # Extract the link to the full article
+        article_link = most_recent_article.find("a")["href"]
 
-# Make a GET request to the full article page
-full_article_response = requests.get(article_link)
+        # Make a GET request to the full article page
+        full_article_response = requests.get(article_link)
 
-# Parse the HTML content of the full article page
-full_article_soup = BeautifulSoup(full_article_response.content, "html.parser")
+        # Parse the HTML content of the full article page
+        full_article_soup = BeautifulSoup(full_article_response.content, "html.parser")
 
-# Find the content of the full article (replace with your own logic)
-full_article_content = full_article_soup.find("div", class_="single-post__body")
+        # Find the content of the full article (replace with your own logic)
+        full_article_content = full_article_soup.find("div", class_="single-post__body")
 
-article_content = str(full_article_content)
+        article_content = str(full_article_content)
 
-# Translate the article to French using googletrans
-translator = Translator(to_lang="fr")
-translated_article = translator.translate(article_content)
+        # Translate the article to French using googletrans
+        translator = Translator()
+        print("translating article...")
+        translated_article = translator.translate(article_content,dest="fr")
 
-# Write the translated article to a file
-with open("translated_article.txt", "w") as file:
-    file.write(translated_article)
+        # Write the translated article to a file
+        with open("translated_article.txt", "w", encoding="utf-8") as file:
+            file.write(translated_article.text)
+        print("translated article written to file")
+
+    else:
+        print("No new article found today. The last article was published on ", most_recent_date)
+        
+    time.sleep(60)
